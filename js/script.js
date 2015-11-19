8c8fe2172a4f42c17a17f50647e46cb750c98c38
multpix <multpix@gmail.com>
2015-11-19 15:02:22 +0200

    initial

---
+++ js/script.js
@@ -0,0 +1,15 @@
+new Vivus('dru_logo', {
+  type: 'oneByOne',
+  duration: 200,
+  file: 'dru_logo.svg'
+}, doDone());
+
+new Vivus('dru_text', {
+  type: 'oneByOne',
+  duration: 200,
+  file: 'dru_text.svg'
+}, doDone());
+
+function doDone() {
+  console.log('Animation finished.');
+}

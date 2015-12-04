ac1a76b2c8c120c6daae33bc93144bc57231e2aa
multpix <multpix@gmail.com>
2015-12-04 02:05:57 +0200

    New drop shape

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 new Vivus('dru_logo', {
   type: 'oneByOne',
-  duration: 200,
+  duration: 150,
   file: 'dru_logo.svg'
 }, doDone());
 
 new Vivus('dru_text', {
   type: 'oneByOne',
-  duration: 200,
+  duration: 150,
   file: 'dru_text.svg'
 }, doDone());
 
